8bd023858eec2fc6aaa5f8253463db397c75c750
Marianne Guisset <marianneguisset@aol.com>
2023-04-01 19:34:05 +0200

    connexion ok mais y'a pas les droits

--- cultejava_front/src/app/accueil/accueil.component.ts
+++ cultejava_front/src/app/accueil/accueil.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Connexion } from '../modelConnexion';
+import { AccueilHttpService } from './accueil-http.service';
+import { Router } from '@angular/router';
+import { TapoteurRequestResponse } from '../modelTapoteur';
 
 @Component({
   selector: 'accueil',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent {
+  connexionForm: Connexion = new Connexion();
+  connected: TapoteurRequestResponse;
+  wrongPassword: boolean = false;
 
+  constructor(private connexionService: AccueilHttpService, private router: Router){}
+
+  connexion(): void{
+    console.log ("test" + this.connexionForm);
+    this.connexionService.findByLogin(this.connexionForm).subscribe(resp => {
+      this.connected = resp;})
+    
+      if(this.connected != null){
+        this.router.navigate(['/tapoteur']);
+      }else{
+        this.wrongPassword = true;
+      }
+  }
 }

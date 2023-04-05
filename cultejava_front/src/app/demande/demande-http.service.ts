30bd9376b84d60fee253a012a2007898d8b54c45
utopiathe <anthony.rouyer33.16@gmail.com>
2023-04-05 11:00:32 +0200

    crud de base demandes (non focntionnel)

--- cultejava_front/src/app/demande/demande-http.service.ts
+++ cultejava_front/src/app/demande/demande-http.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { Demande } from "../modelDemande";
+import { Observable } from "rxjs/internal/Observable";
+import { LoginService } from "../login.service";
+import { TapoteurRequestResponse } from "../modelTapoteur";
 
 @Injectable({
     providedIn: 'root'
@@ -8,7 +12,50 @@ import { environment } from "src/environments/environment";
   export class DemandeHttpService {
     private connexionApiPath: string;
   
-    constructor(private http: HttpClient) {
-      this.connexionApiPath = environment.apiUrl + "/tapoteur/connexion";
+    mesDemandes : Array<Demande> =new Array<Demande>(); //demande d'un indenteur
+    demandes: Array<Demande> = new Array<Demande>(); //demande pour le grand dev
+    private demandeApiPath: string; //chemin
+  
+    constructor(private http: HttpClient, private loginService: LoginService) {
+      this.demandeApiPath = environment.apiUrl + '/demandes'; // pour aller a demandes
+      this.load();
     }
-} 
+  
+    findAll(): Array<Demande> {
+      return this.demandes;
+    }
+  
+    findById(id: number): Observable<Demande> {
+      return this.http.get<Demande>(this.demandeApiPath + "/" + id);
+    }
+  
+    create(demande: Demande): void {
+    
+      this.http.post<Demande>(this.demandeApiPath, demande).subscribe(resp => {
+        this.load();
+      });
+    }
+  
+    update(demande: Demande): void {
+      this.http.put<Demande>(this.demandeApiPath + "/" +  demande.id, demande).subscribe(resp => {
+        this.load();
+      });
+    }
+  
+    remove(id: number): void {
+      this.http.delete<boolean>(this.demandeApiPath + "/" + id).subscribe(resp => {
+        this.load();
+      });
+    }
+
+    private load(): void {
+        this.http.get<Array<Demande>>(this.demandeApiPath + "/by-fidele/" + this.connected().id).subscribe(resp => {
+        this.demandes = resp;
+        });
+    } 
+
+    connected():TapoteurRequestResponse{
+        return this.loginService.connected;
+      }
+  
+}
--- cultejava_front/src/app/demande/demande.component.ts
+++ cultejava_front/src/app/demande/demande.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
+import { Demande } from '../modelDemande';
+import { DemandeHttpService } from './demande-http.service';
 
 @Component({
   selector: 'app-demande',
@@ -6,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./demande.component.scss']
 })
 export class DemandeComponent {
+  demandeForm: Demande = null;
 
+  constructor(private demandeService: DemandeHttpService, private loginService: LoginService, private router: Router) {
+    if(this.loginService.connected == null){
+      this.router.navigate([""]);
+    }
+  }
+
+  list(): Array<Demande> {
+    return this.demandeService.findAll();
+  }
+ 
+  add(): void {
+    this.demandeForm = new Demande();
+  }
+
+  edit(id: number): void {
+    this.demandeService.findById(id).subscribe((resp: Demande) => {
+      this.demandeForm = resp;
+    });
+  }
+
+  remove(id: number): void {
+    this.demandeService.remove(id);
+  }
+
+  save(): void {
+    if(this.demandeForm.id) {
+      this.demandeService.update(this.demandeForm);
+    } else {
+      this.demandeService.create(this.demandeForm);
+    }
+
+    this.cancel();
+  }
+
+  cancel(): void {
+    this.demandeForm = null;
+  }
 }
